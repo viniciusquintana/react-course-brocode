5a1cd90515cc210de7c637ebb2f5af5faa71a263
viniciusquintana <137747453+viniciusquintana@users.noreply.github.com>
2024-12-23 15:48:41 -0300

    finish CSS styles lesson

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,7 @@
-import Card from "./Card.jsx"
+import Button from "./Button.jsx";
 
 function App() {
-  return(
-    <>
-      <Card/>
-    </>
-    
-  );
+  return(<Button/>);
 }
 
 export default App
