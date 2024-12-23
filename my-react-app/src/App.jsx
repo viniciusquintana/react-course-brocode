32a86cee22020cb4000eed63018f20c6b7217763
viniciusquintana <137747453+viniciusquintana@users.noreply.github.com>
2024-12-23 16:45:26 -0300

    finish conditional rendering lesson

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,13 +1,9 @@
-import Student from "./Student.jsx";
+import UserGreeting from "./UserGreeting.jsx";
 
 function App() {
   return(
     <>
-      <Student name="Spongebob" age={30} isStudent={true}/>
-      <Student name="Patrick" age={42} isStudent={false}/>
-      <Student name="Squidward" age={50} isStudent={false}/>
-      <Student name="Sandy" age={27} isStudent={true}/>
-      <Student name="Larry"/>
+      <UserGreeting isLoggedIn={true} username="Vinny"/>
     </>
   );
 }
