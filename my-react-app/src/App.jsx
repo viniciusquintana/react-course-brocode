abf19c1cfda9e5d8c6c14a76d8e9f3e0c77c8c6c
viniciusquintana <137747453+viniciusquintana@users.noreply.github.com>
2024-12-23 16:23:27 -0300

    finish props lesson

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,7 +1,15 @@
-import Button from "./Button.jsx";
+import Student from "./Student.jsx";
 
 function App() {
-  return(<Button/>);
+  return(
+    <>
+      <Student name="Spongebob" age={30} isStudent={true}/>
+      <Student name="Patrick" age={42} isStudent={false}/>
+      <Student name="Squidward" age={50} isStudent={false}/>
+      <Student name="Sandy" age={27} isStudent={true}/>
+      <Student name="Larry"/>
+    </>
+  );
 }
 
 export default App
