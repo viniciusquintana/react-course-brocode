48487f8c711e35714aac49031031906ffa151297
viniciusquintana <137747453+viniciusquintana@users.noreply.github.com>
2024-12-24 17:46:26 -0300

    finish update arrays in state lesson

--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -3,27 +3,31 @@
 import React, {useState} from 'react';
 
 function MyComponent(){
-  
-  const [car, setCar] = useState({year: 2000, 
-                                  make: "Nissan", 
-                                  model: "Skyline GT-R"});
 
-  function handleYearChange(event){
-    setCar(c => ({...c, year: event.target.value}));
+  const [foods, setFoods] = useState(['Apple', 'Orange', 'Banana']);
+
+  function handleAddFood(){
+    
+    const newFood = document.getElementById('foodInput').value;
+    document.getElementById('foodInput').value = "";
+
+    setFoods(f => [...f, newFood]);
   }
-  function handleMakeChange(event){
-    setCar(c => ({...c, make: event.target.value}));
-  }
-  function handleModelChange(event){
-    setCar(c => ({...c, model: event.target.value}));
+  function handleRemoveFood(index){
+
+    setFoods(foods.filter((_, i) => i !== index));
   }
 
-  return  (<div>
-            <p>Your favorite car is: {car.year} {car.make} {car.model}</p>
-
-            <input type='number' value={car.year} onChange={handleYearChange}/><br/>
-            <input type='text' value={car.make} onChange={handleMakeChange}/><br/>
-            <input type='text' value={car.model} onChange={handleModelChange}/><br/>
+  return (<div>
+            <h2>List of Food</h2>
+            <ul>
+              {foods.map((food, index) => 
+              <li key={index} onClick={() => handleRemoveFood(index)}>
+                {food}
+              </li>)}
+            </ul>
+            <input type='text' id='foodInput' placeholder='Enter food name'/>
+            <button onClick={handleAddFood}>Add Food</button>
           </div>);
 }
 
