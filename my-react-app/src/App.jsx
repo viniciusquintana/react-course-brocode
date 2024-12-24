425c6d84fbd483af79713b50462801ab85e706d1
viniciusquintana <137747453+viniciusquintana@users.noreply.github.com>
2024-12-24 17:13:58 -0300

    finish updater function lesson

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,8 +1,8 @@
-import ColorPicker from "./ColorPicker.jsx";
+import MyComponent from "./MyComponent.jsx";
 
 function App() {
 
-  return(<ColorPicker />)
+  return(<MyComponent />)
 }
 
 export default App;
--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -4,64 +4,30 @@ import React, {useState} from 'react';
 
 function MyComponent(){
 
-  const [name, setName] = useState("Guest");
-  const [quantity, setQuantity] = useState(1);
-  const [comment, setComment] = useState("");
-  const [payment, setPayment] = useState("");
-  const [shipping, setShipping] = useState("Delivery");
+  const [count, setCount] = useState(0);
 
-  function handleNameChange(event) {
-    setName(event.target.value);
+  const increment = () => {
+    setCount(c => c + 1);
+    setCount(c => c + 1);
+    setCount(c => c + 1);
   }
 
-  function handleQuantityChange(event) {
-    setQuantity(event.target.value);
+  const decrement = () => {
+    setCount(c => c - 1);
+    setCount(c => c - 1);
+    setCount(c => c - 1);
   }
 
-  function handleCommentChange(event) {
-    setComment(event.target.value);
+  const reset = () => {
+    setCount(0);
   }
 
-  function handlePaymentChange(event) {
-    setPayment(event.target.value);
-  }
-
-  function handleShippingChange(event) {
-    setShipping(event.target.value);
-  }
-
-  return(<div>
-    <input value={name} onChange={handleNameChange}/>
-    <p>Name: {name}</p>
-
-    <input value={quantity} onChange={handleQuantityChange} type='number'/>
-    <p>Quantity: {quantity}</p>
-
-    <textarea value={comment} onChange={handleCommentChange}
-    placeholder='Enter delivery instructions'/>
-    <p>Comment: {comment}</p>
-
-    <select value={payment} onChange={handlePaymentChange}>
-      <option value="">Select an option</option>
-      <option value="Visa">Visa</option>
-      <option value="Mastercard">Mastercard</option>
-      <option value="Giftcard">Giftcard</option>
-    </select>
-    <p>Payment: {payment}</p>
-
-    <label>
-      <input  type='radio' value='Pick Up'
-              checked={shipping === 'Pick Up'}
-              onChange={handleShippingChange}/>
-      Pick Up
-    </label> <br/>
-    <label>
-    <input  type='radio' value='Delivery'
-              checked={shipping === 'Delivery'}
-              onChange={handleShippingChange}/>
-      Delivery
-    </label>
-    <p>Shipping: {shipping}</p>
-  </div>);
+  return( <div >
+            <p>Count: {count}</p>
+            <button onClick={decrement}>Decrement</button>
+            <button onClick={reset}>Reset</button>
+            <button onClick={increment}>Increment</button>
+          </div>);
 }
+
 export default MyComponent
